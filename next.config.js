400624820492dc055823c39039a3b59d42fad98b
Thomas <47186265+TBillaudeau@users.noreply.github.com>
2025-11-25 23:36:59 +0100

    2.5.3

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone', // Enable standalone build for Docker
   reactStrictMode: true,
   images: {
     remotePatterns: [
